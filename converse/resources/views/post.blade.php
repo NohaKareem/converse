d716d176b33e2b1d8fd1eb9c117b914c7f961ec6
Noha Kareem <NohaKareem@users.noreply.github.com>
2019-12-16 05:38:39 -0500

    Added comment display

--- converse/resources/views/post.blade.php
+++ converse/resources/views/post.blade.php
@@ -15,15 +15,23 @@
                         @csrf
                         <button class="btn btn-outline-danger btn-sm" type="submit">delete</button>
                     </form>
-            
-                <form action="/comments" method="POST">
-                    @csrf
-                    <label for="commentText" class="control-label sr-only">Post comment</label>
-                    <textarea rows="10" name="commentText" id="commentText" class="form-control" placeholder="comment"></textarea>  
-                    <!-- pass post id -->
-                    <input rows="10" name="post_id" id="post_id" class="form-control hidden" type="text" placeholder="post id" value="{{$post->id}}">
-                    <button class="btn btn-outline-success btn-sm actionButton" type="submit">post comment</button>
-                </form>
+                    
+                    <h2>Comments</h2>
+                    <!-- display post comments -->
+                    @foreach($post->comments as $comment) 
+                        <p class="commentText">{{ $comment->commentText }} </p>
+                        <p class="commentAuthor">By <a href="/users/{{$comment->user_id}}"> {{ App\User::find($comment->user_id)->first_name }} {{ App\User::find($comment->user_id)->last_name }} </a> </p>
+                    @endforeach
+
+                    <h3>Add comment</h3>
+                    <form action="/comments" method="POST">
+                        @csrf
+                        <label for="commentText" class="control-label sr-only">Post comment</label>
+                        <textarea rows="3" name="commentText" id="commentText" class="form-control" placeholder="comment"></textarea>  
+                        <!-- pass post id -->
+                        <input name="post_id" id="post_id" class="form-control hidden" type="text" placeholder="post id" value="{{$post->id}}">
+                        <button class="btn btn-outline-success btn-sm actionButton" type="submit">post comment</button>
+                    </form>
             </div>
             @endforeach
         </div>  
