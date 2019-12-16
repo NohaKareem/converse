a24167f9a753796e5744967bd4c01d153c80dd02
Noha Kareem <NohaKareem@users.noreply.github.com>
2019-12-16 05:47:48 -0500

    Updated styling and comment feature

--- converse/resources/views/post.blade.php
+++ converse/resources/views/post.blade.php
@@ -13,25 +13,36 @@
                     <form action="/posts/{{ $post->id }}" method="POST">
                         @method('DELETE')
                         @csrf
-                        <button class="btn btn-outline-danger btn-sm" type="submit">delete</button>
+                        <button class="btn btn-outline-danger btn-sm" type="submit">delete post</button>
                     </form>
                     
-                    <h2>Comments</h2>
-                    <!-- display post comments -->
-                    @foreach($post->comments as $comment) 
-                        <p class="commentText">{{ $comment->commentText }} </p>
-                        <p class="commentAuthor">By <a href="/users/{{$comment->user_id}}"> {{ App\User::find($comment->user_id)->first_name }} {{ App\User::find($comment->user_id)->last_name }} </a> </p>
-                    @endforeach
+                    <div class="commentSection">
+                        <h2>Comments</h2>
+                        <!-- display post comments -->
+                        @foreach($post->comments as $comment) 
+                            <p class="commentText">{{ $comment->commentText }} </p>
+                            <p class="commentAuthor">By <a href="/users/{{$comment->user_id}}"> {{ App\User::find($comment->user_id)->first_name }} {{ App\User::find($comment->user_id)->last_name }} </a> </p>
+                            
+                            @if(auth()->id() == $comment->user_id)
+                                <form action="/comments/{{ $comment->id }}" method="POST">
+                                    @method('DELETE')
+                                    @csrf
+                                    <button class="btn btn-outline-danger btn-sm inlineButton" type="submit">delete comment</button>
+                                </form>
+                            @endif
+                        @endforeach
 
-                    <h3>Add comment</h3>
-                    <form action="/comments" method="POST">
-                        @csrf
-                        <label for="commentText" class="control-label sr-only">Post comment</label>
-                        <textarea rows="3" name="commentText" id="commentText" class="form-control" placeholder="comment"></textarea>  
-                        <!-- pass post id -->
-                        <input name="post_id" id="post_id" class="form-control hidden" type="text" placeholder="post id" value="{{$post->id}}">
-                        <button class="btn btn-outline-success btn-sm actionButton" type="submit">post comment</button>
-                    </form>
+                        <h3>Add comment</h3>
+                        <form action="/comments" method="POST">
+                            @csrf
+                            <label for="commentText" class="control-label sr-only">Post comment</label>
+                            <textarea rows="3" name="commentText" id="commentText" class="form-control" placeholder="comment"></textarea>  
+                            
+                            <!-- pass post id -->
+                            <input name="post_id" id="post_id" class="form-control hidden" type="text" placeholder="post id" value="{{$post->id}}">
+                            <button class="btn btn-outline-success btn-sm actionButton" type="submit">post comment</button>
+                        </form>
+                    </div>
             </div>
             @endforeach
         </div>  
