0aac476fa271dd1a0cfe15ab4e879f6880241d08
Noha Kareem <NohaKareem@users.noreply.github.com>
2019-12-16 06:03:28 -0500

    Fix image display bug

--- converse/database/seeds/DatabaseSeeder.php
+++ converse/database/seeds/DatabaseSeeder.php
@@ -12,8 +12,13 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        // disable foreign key checks before callign seeders for post and comment seeders
+        // https://stackoverflow.com/a/28005195/1446598
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
         $this->call(UserSeeder::class);
         $this->call(PostSeeder::class);
         $this->call(CommentSeeder::class);
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
     }
+    
 }
