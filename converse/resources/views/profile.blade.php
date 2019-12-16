9209a4fe8bed917a458db98e2bbdc9406823b8e0
Noha Kareem <NohaKareem@users.noreply.github.com>
2019-12-16 08:24:22 -0500

    Added view posts per author (user) feature, with authentication-conditioned delete

--- converse/resources/views/profile.blade.php
+++ converse/resources/views/profile.blade.php
@@ -8,6 +8,19 @@
                 <div class="profileInfo">
                     <p class="userName">{{ $user->first_name.' '.$user->last_name }}</p>
                     <p>{{ $user->email }}</p>
+
+                    <h2>{{ $user->first_name.' '.$user->last_name  }}'s posts</h2>
+                    @foreach($user->posts as $post) 
+                        <p class="postLink"><a href="/posts/{{$post->id}}"> {{ App\Post::find($post->id)->title }} </a> </p>
+                        @if(auth()->id() == $user->id)
+                            <form action="/posts/{{ $post->id }}" method="POST">
+                                @method('DELETE')
+                                @csrf
+                            <button class="btn btn-outline-danger btn-sm inlineButton" type="submit">delete post</button>
+                            </form>
+                        @endif
+                   
+                    @endforeach
                 </div>
            </div>
     </div>
